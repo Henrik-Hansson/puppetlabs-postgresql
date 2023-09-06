6db39999cf1b38a1b604b369550fc7f2848e4383
Simon Hoenscheid <simon@hoenscheid-itconsulting.com>
2023-09-06 14:04:57 +0200

    PDK update 3.0.0

--- lib/puppet/type/postgresql_psql.rb
+++ lib/puppet/type/postgresql_psql.rb
@@ -148,7 +148,7 @@ Puppet::Type.newtype(:postgresql_psql) do
 
   private
 
-  def set_sensitive_parameters(sensitive_parameters) # rubocop:disable Style/AccessorMethodName
+  def set_sensitive_parameters(sensitive_parameters)
     # Respect sensitive commands
     if sensitive_parameters.include?(:unless)
       sensitive_parameters.delete(:unless)
